6ddba2246716fa44556e9cffb6814454b34a0537
nandeeshnn77-lab <nandeeshnn77@gmail.com>
2026-08-26 15:42:49 +0530

    Add internet gateway resource to main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,10 @@ resource "aws_vpc" "main" {
     Name = "${var.project_name}-vpc"
   }
 }
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
