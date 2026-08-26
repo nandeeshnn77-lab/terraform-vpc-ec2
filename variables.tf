e35daac1ee67e8658b47f9950c63f076dab6a296
nandeeshnn77-lab <nandeeshnn77@gmail.com>
2026-08-26 15:41:03 +0530

    Add AWS configuration variables in variables.tf
    Defines variables for AWS configuration including region, VPC CIDR, public subnet CIDR, availability zone, instance type, and project name.

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR range for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR range for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+  default     = "ap-south-1a"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "terraform-demo"
+}
